98bd348787013a2d52623060d24281b5730f5355
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-10 10:09:24 +0900

    余分なコメントを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # This line mounts Solidus's routes at the root of your application.
-  # This means, any requests to URLs such as /products, will go to Spree::ProductsController.
-  # If you would like to change where this engine is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Solidus relies on it being the default of "spree"
   mount Spree::Core::Engine, at: '/'
 
   namespace :potepan do
@@ -26,5 +21,4 @@ Rails.application.routes.draw do
     get :privacy_policy,            to: 'sample#privacy_policy'
     get 'products',                 to: 'products#show'
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
