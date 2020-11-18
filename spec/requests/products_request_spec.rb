d4764cdabdb8d4e4e9277894b0b873512664e34b
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-18 23:21:01 +0900

    商品詳細ページのリクエストテストを追加

--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
-
+  describe "GET/show" do
+    let(:product) { create(:product) }
+    it '商品詳細画面の表示に成功すること' do
+    get potepan_product_path(product.id)
+    expect(response).to have_http_status(200)
+    end
+  end
 end
