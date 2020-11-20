b21890b77272b7fc9af002ba371120f68c84ffce
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-20 23:50:50 +0900

    変数の値が空白の場合を想定しblank?メソッドに変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def full_title(page_title='')
     base_title = 'BIGBAG'
-    if page_title.nil?
+    if page_title.blank?
       base_title
     else
       "#{page_title} | #{base_title}"
