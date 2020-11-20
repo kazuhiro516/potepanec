f9fe01edcaa5a7270f8fc03b542b2170625a876b
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-20 23:47:30 +0900

    値がnilの場合のテストを追加

--- spec/helpers/application_helper.rb
+++ spec/helpers/application_helper.rb
@@ -4,7 +4,8 @@ RSpec.describe ApplicationHelper, type: :helper do
   describe "#full_title" do
     it "タイトルが正常に表示されること" do
       expect(full_title("test")).to eq "test | BIGBAG"
-      expect(full_title("")).to eq " | BIGBAG"
+      expect(full_title("")).to eq "BIGBAG"
+      expect(full_title()).to eq "BIGBAG"
     end
   end
 end
