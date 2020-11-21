f9a9717b9cae21bd230302944d35196125ada04c
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-21 17:32:24 +0900

    期待値をresponse.bodyに変更

--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -1,23 +1,25 @@
-require 'rails_helper'
-
 RSpec.describe "Products", type: :request do
   describe "GET/show" do
     let(:product) { create(:product) }
-    it '商品詳細画面の表示に成功すること' do
+
+    before do
       get potepan_product_path(product.id)
+    end
+
+    it '商品詳細画面の表示に成功すること' do
       expect(response).to have_http_status(200)
     end
+
     it '商品名が動的に表示されていること' do
-      expect(product.name).to eq product.name
+      expect(response.body).to include product.name
     end
+
     it '商品価格が動的に表示されていること' do
-      expect(product.price).to eq product.price
+      expect(response.body).to include product.display_price.to_s
     end
+
     it '商品内容が動的に表示されていること' do
-      expect(product.description).to eq product.description
-    end
-    it '商品画像が動的に表示されていること' do
-      expect(product.images).to eq product.images
+      expect(response.body).to include product.description
     end
   end
 end
