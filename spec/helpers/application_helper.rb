8682afc7e36e16a8a19767a9913465bc606aa472
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-23 15:47:12 +0900

    実際の値を定数に変更

--- spec/helpers/application_helper.rb
+++ spec/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 RSpec.describe ApplicationHelper, type: :helper do
+  let(:base_title) { 'BIGBAG'.freeze }
+
   describe "#full_title" do
     it "タイトルが正常に表示されること" do
-      expect(full_title("test")).to eq "test | BIGBAG"
-      expect(full_title("")).to eq "BIGBAG"
-      expect(full_title).to eq "BIGBAG"
+      expect(full_title("test")).to eq "test | #{base_title}"
+      expect(full_title("")).to eq "#{base_title}"
+      expect(full_title(nil)).to eq "#{base_title}"
     end
   end
 end
