329f6343011c27f1cf6418bbaf695db56b387919
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-24 11:59:35 +0900

    理解しやすくするため説明を日本語に変更

--- spec/helpers/application_helper.rb
+++ spec/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 RSpec.describe ApplicationHelper, type: :helper do
   let(:base_title) { 'BIGBAG'.freeze }
 
-  describe "#full_title" do
+  describe "タイトルのテスト" do
     it "タイトルが正常に表示されること" do
       expect(full_title("test")).to eq "test | #{base_title}"
       expect(full_title("")).to eq "#{base_title}"
