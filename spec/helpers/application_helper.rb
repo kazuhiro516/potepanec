dcd69dc92a5f0f6bc8f06bf7285cbd9fa68d5d63
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-26 00:36:29 +0900

    途中でテストが失敗してしまうとその後のexpextがパスするか不明なため。

--- spec/helpers/application_helper.rb
+++ spec/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 RSpec.describe ApplicationHelper, type: :helper do
   let(:base_title) { 'BIGBAG'.freeze }
 
-  describe "タイトルのテスト" do
-    it "タイトルが正常に表示されること" do
+  describe 'タイトルのテスト' do
+    it 'タイトルが正常に表示されること' do
       expect(full_title("test")).to eq "test | #{base_title}"
+    end
+    it '値が文字列の場合base_titleを返す' do
       expect(full_title("")).to eq "#{base_title}"
+    end
+    it '値がない場合base_titleを返す' do
       expect(full_title(nil)).to eq "#{base_title}"
     end
   end
