82a141cdccd44367177e55ac65c95c98fc030a80
黒川和弘 <k.kurokawa11@gmail.com>
2020-11-30 07:35:46 +0900

    HOMEリンクのエラーを解消するため実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,16 @@ module ApplicationHelper
     if page_title.blank?
       BASE_TITLE
     else
-      "#{page_title} | #{BASE_TITLE}"
+      "#{page_title} - #{BASE_TITLE}"
+    end
+  end
+
+  def heading_title(page_heading)
+    if page_heading.present? && page_heading.include?("/")
+      page_heading.split("/")
+    else
+      heading_array = []
+      heading_array.push(page_heading)
     end
   end
 end
