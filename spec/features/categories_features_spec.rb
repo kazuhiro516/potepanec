ef102ead6ce2d726569ea6c29ea9b883ceabba8b
黒川和弘 <k.kurokawa11@gmail.com>
2020-12-10 19:47:01 +0900

    テスト効率を上げるため

--- spec/features/categories_features_spec.rb
+++ spec/features/categories_features_spec.rb
@@ -7,35 +7,22 @@ RSpec.feature "Categories", type: :feature do
     visit potepan_category_path(taxon.id)
   end
 
-  scenario '現在のパスが等しいことを検証する' do
-    expect(current_path).to eq potepan_category_path(taxon.id)
-  end
-
   scenario 'タイトルの要素を検証する' do
     expect(page).to have_title taxon.name
   end
 
-  scenario 'HOMEへのリンクが表示されていること' do
+  scenario 'リンクが表示されていること' do
     expect(page).to have_link 'HOME'
   end
 
-  scenario '各カテゴリーへのリンクが表示されていること' do
-    expect(page).to have_link taxonomy.name
-  end
-
-  scenario '各商品へのリンクが表示されていること' do
+  scenario 'カテゴリーに属する商品が表示されている' do
     expect(page).to have_link product.name
-  end
-
-  scenario '商品名が表示されている' do
     expect(page).to have_selector '.productBox h5', text: product.name
-  end
-
-  scenario '値段が表示されている' do
     expect(page).to have_selector '.productBox h3', text: product.display_price
   end
 
-  scenario '文字列が表示されていること' do
+  scenario 'サイドバーにカテゴリーが表示されていること' do
     expect(page).to have_content '商品カテゴリー'
+    expect(page).to have_link taxonomy.name
   end
 end
--- spec/features/products_features_spec.rb
+++ spec/features/products_features_spec.rb
@@ -7,10 +7,6 @@ RSpec.feature "Products", type: :feature do
     visit potepan_product_path(product.id)
   end
 
-  scenario '一覧ページへのリンクが表示されていること' do
-    expect(page).to have_link '一覧ページへ戻る'
-  end
-
   scenario 'HOMEへのリンクが表示されていること' do
     expect(page).to have_link 'HOME'
   end
@@ -19,19 +15,10 @@ RSpec.feature "Products", type: :feature do
     expect(page).to have_title product.name
   end
 
-  scenario '商品名が表示されている' do
+  scenario '商品詳細が表示されている' do
     expect(page).to have_selector '.media-body h2', text: product.name
-  end
-
-  scenario '値段が表示されている' do
     expect(page).to have_selector '.media-body h3', text: product.display_price
-  end
-
-  scenario '商品説明が表示されている' do
     expect(page).to have_selector '.media-body p', text: product.description
-  end
-
-  scenario '現在のパスが等しいことを検証する' do
-    expect(current_path).to eq potepan_product_path(product.id)
+    expect(page).to have_link '一覧ページへ戻る'
   end
 end
