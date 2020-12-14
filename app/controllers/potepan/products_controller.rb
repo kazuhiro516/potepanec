a686314880a07a5d4c74d45552c04d7fca9ff2ed
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-15 00:20:24 +0900

    関連商品を実装

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,9 @@
 class Potepan::ProductsController < ApplicationController
+  MAX_RELATED_PRODUCT = 4
+
   def show
     @product = Spree::Product.find(params[:id])
     @taxons = @product.taxons
+    @related_products = Spree::Product.related_products(@product).limit(MAX_RELATED_PRODUCT)
   end
 end
