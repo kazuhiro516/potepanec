e912164f02cab7e7eea3237661a6f5ba923936bb
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-17 22:55:51 +0900

    不要なファイルを削除

--- app/models/potepan/product_decorator.rb
+++
@@ -1,6 +0,0 @@
-Spree::Product.class_eval do
-  scope :related_products, -> (product) do
-    in_taxons(product.taxons).includes(master: [:default_price, :images]).
-      where.not(id: product.id).distinct
-  end
-end
