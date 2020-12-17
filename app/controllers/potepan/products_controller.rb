2b6ce3c30768f06534742ecab8031e010a2c33c6
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-17 14:27:39 +0900

    定数名を変更

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,10 +1,10 @@
 class Potepan::ProductsController < ApplicationController
-  MAX_RELATED_PRODUCT = 4
-  MAX_RELATED_PRODUCT.freeze
+  MAX_RELATED_PRODUCT_COUNT = 4
+  MAX_RELATED_PRODUCT_COUNT.freeze
 
   def show
     @product = Spree::Product.find(params[:id])
     @taxons = @product.taxons
-    @related_products = @product.related_products.limit(MAX_RELATED_PRODUCT)
+    @related_products = @product.related_products.limit(MAX_RELATED_PRODUCT_COUNT)
   end
 end
