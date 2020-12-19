14e146677771eab90777878e25c924fe954da14d
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-19 23:42:14 +0900

    関連しない商品を作成し、テストを追加

--- spec/models/product_decorator_spec.rb
+++ spec/models/product_decorator_spec.rb
@@ -2,7 +2,9 @@ RSpec.describe Spree::ProductDecorator, type: :model do
   describe 'related_productsのテスト' do
     let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root) }
+    let(:taxon_test) { create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root) }
     let(:product) { create(:product, taxons: [taxon]) }
+    let(:product_test) { create(:product, taxons: [taxon_test]) }
     let!(:related_products) { create_list(:product, 4, taxons: [taxon]) }
     let!(:related_products_test) { create_list(:product, 5, taxons: [taxon]) }
 
@@ -11,15 +13,11 @@ RSpec.describe Spree::ProductDecorator, type: :model do
     end
 
     it '関連しない商品が含まれていないこと' do
-      expect(product.related_products).not_to include product.taxons
+      expect(product.related_products).not_to include product_test.taxons
     end
 
     it '自商品が含まれていないこと' do
       expect(product.related_products).not_to include product
-    end
-
-    it '関連商品が4件に制限されること' do
-      expect(product.related_products).not_to eq related_products_test
     end
   end
 end
