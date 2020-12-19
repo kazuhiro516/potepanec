3abcdd9eb333c27abb7c1ecd13d820f6fdda9a86
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-19 13:32:11 +0900

    テストを追加

--- spec/models/product_decorator_spec.rb
+++ spec/models/product_decorator_spec.rb
@@ -4,9 +4,22 @@ RSpec.describe Spree::ProductDecorator, type: :model do
     let(:taxon) { create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root) }
     let(:product) { create(:product, taxons: [taxon]) }
     let!(:related_products) { create_list(:product, 4, taxons: [taxon]) }
+    let(:related_products_test) { create_list(:product, 5, taxons: [taxon]) }
 
     it '定数に指定した件数のみ取得できていること' do
       expect(product.related_products.count).to eq 4
+    end
+
+    it '関連しない商品が含まれていないこと' do
+      expect(product.related_products).not_to include taxon
+    end
+
+    it '自商品が含まれていないこと' do
+      expect(product.related_products).not_to include product
+    end
+
+    it '関連商品が4件に制限されること' do
+      expect(product.related_products).not_to eq related_products_test
     end
   end
 end
