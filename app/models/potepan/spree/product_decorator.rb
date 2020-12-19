65b1285bd0ff943960a9c69916031adef7f0958a
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-19 16:35:29 +0900

    solidusユーザーガイドに従いselfに変更

--- app/models/potepan/spree/product_decorator.rb
+++ app/models/potepan/spree/product_decorator.rb
@@ -1,8 +1,7 @@
 module Spree::ProductDecorator
-  Spree::Product.class_eval do
-    def related_products
-      Spree::Product.in_taxons(taxons).includes(master: [:default_price, :images]).
-        where.not(id: id).distinct
-    end
+  def related_products
+    Spree::Product.in_taxons(taxons).includes(master: [:default_price, :images]).
+      where.not(id: id).distinct
   end
+  Spree::Product.prepend self
 end
