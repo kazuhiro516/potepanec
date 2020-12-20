ad5e0c04f15cf48908202432dc20a7dcc8665f59
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-20 11:36:41 +0900

    変数名の変更、不要な変数の削除

--- spec/models/product_decorator_spec.rb
+++ spec/models/product_decorator_spec.rb
@@ -2,18 +2,17 @@ RSpec.describe Spree::ProductDecorator, type: :model do
   describe 'related_productsのテスト' do
     let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root) }
-    let(:taxon_test) { create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root) }
+    let(:taxon_unrelated) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
     let(:product) { create(:product, taxons: [taxon]) }
-    let(:product_test) { create(:product, taxons: [taxon_test]) }
+    let(:product_unrelated) { create(:product, taxons: [taxon_unrelated]) }
     let!(:related_products) { create_list(:product, 4, taxons: [taxon]) }
-    let!(:related_products_test) { create_list(:product, 5, taxons: [taxon]) }
 
     it '定数に指定した件数のみ取得できていること' do
       expect(related_products.count).to eq 4
     end
 
     it '関連しない商品が含まれていないこと' do
-      expect(product.related_products).not_to include product_test.taxons
+      expect(product.related_products).not_to include product_unrelated.taxons
     end
 
     it '自商品が含まれていないこと' do
