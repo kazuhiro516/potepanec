8f1b91f3dbc11d0437b2aa055de610a2fe5a27e7
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-20 13:42:23 +0900

    関連商品の取得を4つに制限していることを確認するテストを追加

--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe "Products", type: :request do
     let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root) }
     let(:product) { create(:product, taxons: [taxon]) }
+    let(:unrelated_products) { create_list(:product, 5, taxons: [taxon]) }
 
     it '正常にレスポンスが返ってくること' do
       expect(response).to be_success
@@ -26,6 +27,10 @@ RSpec.describe "Products", type: :request do
 
     it '商品内容が含まれていること' do
       expect(response.body).to include product.description
+    end
+
+    it '関連商品の取得を4つに制限していること' do
+      expect(unrelated_products).not_to eq Potepan::ProductsController::MAX_RELATED_PRODUCT_COUNT
     end
   end
 end
