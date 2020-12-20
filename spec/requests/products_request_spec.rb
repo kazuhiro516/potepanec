c391b899cb84b8741d5bdafd2e72780c62bf5c9d
kurokawakazuhiro <k.kurokawa11@gmail.com>
2020-12-20 13:48:15 +0900

    可読性を上げるため変数名を修正

--- spec/requests/products_request_spec.rb
+++ spec/requests/products_request_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Products", type: :request do
     let(:taxonomy) { create(:taxonomy) }
     let(:taxon) { create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root) }
     let(:product) { create(:product, taxons: [taxon]) }
-    let(:unrelated_products) { create_list(:product, 5, taxons: [taxon]) }
+    let(:related_products) { create_list(:product, 5, taxons: [taxon]) }
 
     it '正常にレスポンスが返ってくること' do
       expect(response).to be_success
@@ -30,7 +30,7 @@ RSpec.describe "Products", type: :request do
     end
 
     it '関連商品の取得を4つに制限していること' do
-      expect(unrelated_products).not_to eq Potepan::ProductsController::MAX_RELATED_PRODUCT_COUNT
+      expect(related_products).not_to eq Potepan::ProductsController::MAX_RELATED_PRODUCT_COUNT
     end
   end
 end
